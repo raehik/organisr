51eaadf715f4396049cd54622a7f0f601887c03c
Ben Orchard <thefirstmuffinman@gmail.com>
2015-10-26 12:47:10 +0000

    SQLiteHelper: many updates

--- include/sqlitehelper.hpp
+++ include/sqlitehelper.hpp
@@ -10,17 +10,28 @@
  */
 class SQLiteHelper {
     public:
-        SQLiteHelper();
+        SQLiteHelper(std::string db_file);
         ~SQLiteHelper();
+
         int init_sqlite_db();
+        void print_db();
 
     private:
-        std::string db_name;
         sqlite3 *db;
+        std::string db_file;
+        int exec_sql(std::string statement);
+        int open_db();
+
+
+        static std::string db_appt;
+
+        static void log_msg(std::string message);
+        static void log_err(std::string message);
         static int callback(void *unused, int argc, char **argv, char **az_col_name);
-        void log_msg(std::string message);
-        void log_err(std::string message);
-        bool file_exists(std::string filename);
+        static bool file_exists(std::string filename);
+        template <typename T> static std::string to_string(T var);
+
+        void step_api_test();
 };
 
 #endif // SQLITEHELPER_H
--- src/sqlitehelper.cpp
+++ src/sqlitehelper.cpp
@@ -1,20 +1,37 @@
 #include "sqlitehelper.hpp"
 #include <iostream>
+#include <sstream>
 #include <stdio.h>
 
-std::string SQLiteHelper::db_name = "test.db";
-sqlite3 * SQLiteHelper::db;
+std::string SQLiteHelper::db_appt = "appointments";
 
-SQLiteHelper::SQLiteHelper() {
-    int rc;
+/**
+ * @brief Initialise a SQLiteHelper.
+ *
+ * The SQLiteHelper class contains methods for manipulating a SQLite database.
+ * It is used by DBHelper.
+ *
+ * @param db_file    The name of the SQLite database file to use.
+ */
+SQLiteHelper::SQLiteHelper(std::string db_file) {
+    this->db_file = db_file;
 
     // check if database file exists
-    if (file_exists(db_name)) {
-        log_msg("Database file exists, not initialising");
+    if (file_exists(this->db_file)) {
+        log_msg("Database file exists -- not initialising");
     } else {
         log_msg("Initialising database");
-        init_sqlite_db();
+        int rc = init_sqlite_db();
+        if (rc != 0) {
+            log_err("Something went wrong, init_sqlite_db returned " + to_string(rc));
+        }
     }
+
+    // connect to database
+    open_db();
+
+    // run some tests
+    step_api_test();
 }
 
 SQLiteHelper::~SQLiteHelper() {
@@ -23,32 +40,34 @@ SQLiteHelper::~SQLiteHelper() {
     sqlite3_close(db);
 }
 
+/**
+ * Convert a variable to a string.
+ */
+template <typename T> std::string SQLiteHelper::to_string(T var) {
+    std::stringstream stream;
+    stream << var;
+    return stream.str();
+}
+
 void SQLiteHelper::log_msg(std::string message) {
     std::cout << message << std::endl;
+}
+
+void SQLiteHelper::log_err(std::string message) {
+    log_msg(message);
 }
 
 /**
  * Check if a file exists.
  */
 bool SQLiteHelper::file_exists(std::string filename) {
-    if (FILE *file = fopen(filename.c_str(), "r")) {
+    char * FILE_READ = (char *)"r";
+    if (FILE *file = fopen(filename.c_str(), FILE_READ)) {
         fclose(file);
         return true;
     } else {
         return false;
     }
-}
-
-/**
- * Callback function for sqlite3_exec().
- */
-int SQLiteHelper::callback(void *unused, int argc, char **argv, char **az_col_name) {
-    int i;
-    for (i = 0; i < argc; i++) {
-        std::cout << az_col_name[i] << " = " << argv[i] ? argv[i] : "NULL";
-    }
-    std::cout << std::endl;
-    return 0;
 }
 
 /**
@@ -62,31 +81,110 @@ int SQLiteHelper::init_sqlite_db() {
     std::string sql_init;
 
     // create database
-    rc = sqlite3_open(db_name.c_str(), &db);
+    rc = sqlite3_open(db_file.c_str(), &db);
 
     // try to open database with sqlite
     if (rc) {
-      log_msg("Can't open database: " + std::string(sqlite3_errmsg(db)));
+      log_err("can't open database: " + std::string(sqlite3_errmsg(db)));
       return 1;
     } else {
-      log_msg("Opened database successfully");
+      log_msg("opened database successfully");
     }
 
     // form SQL statement
-    sql_init = "CREATE TABLE COMPANY(" \
-            "ID            INT  PRIMARY KEY  NOT NULL," \
-            "TITLE         TEXT              NOT NULL," \
-            "DESCRIPTION   TEXT);";
+    sql_init = "create table appointments(" \
+            "id          int  primary key not null," \
+            "title       text             not null," \
+            "description text);";
+
+    //log_msg(sql_init);
 
     // now run it
-    rc = sqlite3_exec(db, sql_init.c_str(), callback, 0, &err_msg);
-    if (rc != SQLITE_OK) {
-        std::cerr << "SQL error: " << err_msg << std::endl;
-        sqlite3_free(err_msg);
+    rc = exec_sql(sql_init);
+
+    if (rc != 0) {
+        log_err("error while executing init SQL");
         return 2;
-    } else {
-        log_msg("Table created successfully");
     }
 
     return 0;
+}
+
+int SQLiteHelper::open_db() {
+    int rc = sqlite3_open(db_file.c_str(), &db);
+    if (rc == SQLITE_OK) {
+        log_msg("opened database successfully");
+        return 0;
+    } else {
+        log_msg("failed to open database");
+        return 1;
+    }
+}
+
+/**
+ * Execute a set of SQL statements.
+ *
+ * @return      non-zero on fail, else 0
+ */
+int SQLiteHelper::exec_sql(std::string statements) {
+    log_msg("exec_sql: beginning");
+    char * err_msg = 0;
+    int rc;
+
+    rc = sqlite3_exec(db, statements.c_str(), callback, 0, &err_msg);
+    if (rc != SQLITE_OK) {
+        log_err("SQL error: " + std::string(err_msg));
+        sqlite3_free(err_msg);
+        return 1;
+    } else {
+        log_msg("exec_sql: operation successful");
+    }
+
+    log_msg("exec_sql: finished");
+    return 0;
+}
+
+/**
+ * SELECT callback function for sqlite3_exec().
+ */
+int SQLiteHelper::callback(void *unused, int argc, char **argv, char **az_col_name) {
+    log_msg("CALLBACK START");
+    for (int i = 0; i < argc; i++) {
+        std::cout << az_col_name[i] << " = " << std::endl << argv[i] ? argv[i] : "null";
+    }
+    std::cout << std::endl;
+    log_msg("CALLBACK END");
+    return 0;
+}
+
+void SQLiteHelper::print_db() {
+    log_msg("Database");
+}
+
+void SQLiteHelper::step_api_test() {
+    char *err_msg;
+    sqlite3_stmt *stmt;
+    std::string *pzTest;
+
+    std::string sql = "select * from " + db_appt;
+
+    int rc = sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, NULL);
+    if (rc != SQLITE_OK) {
+        log_msg("failed to prepare statement");
+        log_msg(to_string(sqlite3_errmsg(db)));
+        sqlite3_finalize(stmt);
+        throw("error");
+    }
+    log_msg("statement prepared successfully");
+
+    do {
+        rc = sqlite3_step(stmt);
+        if (rc == SQLITE_ROW) {
+            log_msg(to_string(sqlite3_column_text(stmt, 0)));
+            log_msg(to_string(sqlite3_column_text(stmt, 1)));
+            log_msg(to_string(sqlite3_column_text(stmt, 2)));
+        }
+    } while (rc == SQLITE_ROW);
+
+    sqlite3_finalize(stmt);
 }
