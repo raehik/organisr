f7416be9a3591d0c6336ce3c180dd72d655d44d0
Ben Orchard <thefirstmuffinman@gmail.com>
2015-11-03 12:01:42 +0000

    LayoutHandler.init_layout(): organise operations

--- src/layouthandler.cpp
+++ src/layouthandler.cpp
@@ -17,8 +17,6 @@ LayoutHandler::LayoutHandler() {
 }
 
 void LayoutHandler::init_layout() {
-    main_win.setFixedSize(initial_win_x, initial_win_y);
-
     QPushButton *button = new QPushButton("ayy lmao", &main_win);
     int b_x = 80;
     int b_y = 30;
@@ -28,13 +26,17 @@ void LayoutHandler::init_layout() {
                         b_x,
                         b_y);
 
-    main_win.show();
-
+    // do logic stuff
     DataHandler db = DataHandler();
     std::vector<Appointment> appts;
     appts.push_back(Appointment("test title 1", "test desc 1"));
     db.insert_appts(appts);
 
+    // set main window layout
     QGridLayout *grid = new QGridLayout(&main_win);
     main_win.setLayout(grid);
+
+    // show the main window
+    main_win.setFixedSize(initial_win_x, initial_win_y);
+    main_win.show();
 }
