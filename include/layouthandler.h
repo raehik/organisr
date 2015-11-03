279bfe5cce655efb33a8b16847e8bc5e3a56d2ca
Ben Orchard <thefirstmuffinman@gmail.com>
2015-11-03 11:34:59 +0000

    move over most GuiHandler code to LayoutHandler

--- include/layouthandler.h
+++ include/layouthandler.h
@@ -1,11 +1,20 @@
 #ifndef LAYOUTHANDLER_H
 #define LAYOUTHANDLER_H
 
+#include <QWidget>
 
 class LayoutHandler
 {
 public:
     LayoutHandler();
+
+    void init_layout();
+
+    static int initial_win_x;
+    static int initial_win_y;
+
+private:
+    QWidget main_win;
 };
 
 #endif // LAYOUTHANDLER_H
