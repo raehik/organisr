fc1ccb5928de951d1d4e03b9268b546d392d9c76
Ben Orchard <thefirstmuffinman@gmail.com>
2015-12-28 17:37:26 +0000

    WidgetPrinter: update header too

--- include/widgetprinter.h
+++ include/widgetprinter.h
@@ -1,11 +1,20 @@
 #ifndef WIDGETPRINTER_H
 #define WIDGETPRINTER_H
 
+#include <QWidget>
+#include <QPrinter>
 
-class WidgetPrinter
+class WidgetPrinter : public QWidget
 {
 public:
-    WidgetPrinter();
+    WidgetPrinter(QWidget *print_object, QString dialog_title);
+    void dialogAndPrint();
+
+private:
+    QWidget *print_object;
+    QPrinter printer;
+    QString opt_dialog_title;
+    QString printer_filename;
 };
 
 #endif // WIDGETPRINTER_H
