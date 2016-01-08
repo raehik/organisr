6e099cb84a3fab87c78e0613be151db363ac5550
Ben Orchard <thefirstmuffinman@gmail.com>
2016-01-08 12:13:59 +0000

    GWinAbout: change win size; add logger

--- src/gwinabout.cpp
+++ src/gwinabout.cpp
@@ -6,24 +6,28 @@
 #include <QVBoxLayout>
 #include <QPushButton>
 
+#include "log.h"
+
+using namespace Util;
+
 GWinAbout::GWinAbout(QWidget *parent) : QDialog(parent) {
     init_win();
 }
 
 void GWinAbout::init_win() {
-    this->resize(300, 0);
+    this->resize(400, 300);
     QVBoxLayout *layout = new QVBoxLayout;
     layout->setMargin(5);
     this->setLayout(layout);
 
-    QString about_text = tr("\
+    QString about_text = "\
 <h1 align=\"center\">About</h1>\
 <p>This software was written for the <b>AQA Computing (2510)</b> A-level course.</p>\
 <p>All source code for this software is available under the MIT license (see \
 LICENSE). You can download the latest version of the source files at the <a \
 href=\"https://github.com/raehik/digital-organiser\">digital-organiser GitHub page</a>.</p>\
 <p>Please see the ATTRIBUTION.md file for attribution and credits.</p>\
-");
+";
     QLabel *about = new QLabel(about_text);
     // open HTML links in default browser
     about->setOpenExternalLinks(true);
