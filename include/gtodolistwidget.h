7d599e5651020d832cd35f43c536939f89c09937
Ben Orchard <thefirstmuffinman@gmail.com>
2016-01-13 09:28:05 +0000

    GTodoListWidget: +refresh() function

--- include/gtodolistwidget.h
+++ include/gtodolistwidget.h
@@ -1,15 +1,28 @@
 #ifndef GTODOLISTWIDGET_H
 #define GTODOLISTWIDGET_H
 
+#include <datahandler.h>
+//#include "gmainwindow.h"
+// forward declaration
+class GMainWindow;
+
+#include <vector>
+#include <string>
 #include <QTextList>
+#include <QLabel>
 #include <QWidget>
 
 
 class GTodoListWidget : public QWidget
 {
 public:
-    GTodoListWidget(QWidget *parent = 0);
+    GTodoListWidget(GMainWindow *parent = 0);
     void build_widget();
+    void refresh();
+
+private:
+    QLabel *todos_label;
+    GMainWindow *parent_win;
 };
 
 #endif // GTODOLISTWIDGET_H
--- src/gtodolistwidget.cpp
+++ src/gtodolistwidget.cpp
@@ -1,12 +1,36 @@
 #include "gtodolistwidget.h"
+#include "gmainwindow.h"
 #include <QTextEdit>
-#include <QPalette>
+#include <string>
+#include <QString>
 #include <QLabel>
+#include <QTextDocument>
 
-GTodoListWidget::GTodoListWidget(QWidget *parent) : QWidget(parent) {
+#include "log.h"
+
+using namespace Util;
+
+GTodoListWidget::GTodoListWidget(GMainWindow *parent) : QWidget(parent) {
+    this->parent_win = parent;
     build_widget();
 }
 
 void GTodoListWidget::build_widget() {
-    QLabel *text = new QLabel("<h2>To-dos</h2><p><ul><li>wow</li></ul></p>", this);
+    todos_label = new QLabel(this);
+    todos_label->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Minimum);
+    todos_label->setWordWrap(true);
+    refresh();
+}
+
+void GTodoListWidget::refresh() {
+    log("refreshing todos");
+    std::vector<std::string> todos = parent_win->get_todos();
+    std::string todo_text = "<h2>To-dos</h2><p><ul>";
+    for (std::vector<std::string>::size_type i = 0; i != todos.size(); i++) {
+        log(todos[i]);
+        std::string tmp = "<li>" + todos[i] + "</li>";
+        todo_text += tmp;
+    }
+    todo_text += "</ul></p>";
+    //todos_label->setText(QString::fromStdString(todo_text));
 }
