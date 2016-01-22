a784389535a91c994163e56063cc2c566a9f130a
Ben Orchard <thefirstmuffinman@gmail.com>
2016-01-22 11:29:15 +0000

    Main: +hack to run tests instead

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include "guihandler.h"
 #include <iostream>
+#include "testsql.h"
 
 int main(int argc, char *argv[]) {
-    GuiHandler gui;
-    return gui.init(argc, argv);
+    //GuiHandler gui;
+    //return gui.init(argc, argv);
+    start_sql_tests();
 }
