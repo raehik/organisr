d8bbfcd9719e947a6a4577e0daf5598ebc919d1b
Ben Orchard <thefirstmuffinman@gmail.com>
2016-01-28 20:11:25 +0000

    testgui: add some tests
    Annoyingly I can't automate GUI testing, so the tests
    are more for automatic or easier placing into the report.

--- src/testgui.cpp
+++ src/testgui.cpp
@@ -13,7 +13,14 @@ int init_gui_tests() {
 
 void test_gui() {
     init_gui_tests();
-    log_test("GUI00", "open ");
+    log_test("GUI00", "add valid appointment with description");
+    log_test("GUI01", "add valid appointment without description");
+    log_test("GUI04", "add appointment without title");
+    // TODO: use ""appt'); drop table appointments;""
+    log_test("GUI05", "appointment SQL injection attack");
+    log_test("GUI10", "");
+
+    // start GUI
     GuiHandler gui;
     char **fake_argv;
     gui.init(0, fake_argv);
