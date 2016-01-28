c55e16fd0f35bfc8876c2265f99671bd0faa9613
Ben Orchard <thefirstmuffinman@gmail.com>
2016-01-28 08:40:04 +0000

    main: update for new testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include "guihandler.h"
 #include <iostream>
+#include "testcommon.h"
 #include "testsql.h"
+#include "testgui.h"
 
 int main(int argc, char *argv[]) {
     //GuiHandler gui;
     //return gui.init(argc, argv);
-    start_sql_tests();
+    Test::init_tests();
+    test_sql();
+    test_gui();
 }
