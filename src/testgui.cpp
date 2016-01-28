4a5766d869abee25829bf4f760e36141e148907e
Ben Orchard <thefirstmuffinman@gmail.com>
2016-01-28 11:00:11 +0000

    testgui: start GUI

--- src/testgui.cpp
+++ src/testgui.cpp
@@ -3,6 +3,7 @@
 #include "dbobject.h"
 #include <fstream>
 #include "sqlitehelper.h"
+#include "guihandler.h"
 
 using namespace Test;
 
@@ -13,4 +14,7 @@ int init_gui_tests() {
 void test_gui() {
     init_gui_tests();
     log_test("GUI00", "open ");
+    GuiHandler gui;
+    char **fake_argv;
+    gui.init(0, fake_argv);
 }
